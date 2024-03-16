27d4411a81fcdb7bf277d6d91058b16c9a89cab8
Susi <102126517+susigith@users.noreply.github.com>
2024-03-16 09:58:31 -0400

    [TEST] agrega Babel para soporte a las importaciones de javascript

---
+++ test/usecases/ask-for-players.test.js
@@ -0,0 +1,7 @@
+import { askForPlayers } from '../../src/usecases';
+
+describe('testing askForPlayers function', () => {
+  test('should return the number of players', () => {
+    // const totalPlayers = askForPlayers();
+  });
+});
