1957bc39cb81ad1865ea9e7a3f5861e111d3e5bd
Susi <102126517+susigith@users.noreply.github.com>
2024-04-04 07:17:03 -0400

    [TEST] funcion renderCard y manejo de excepciones

--- src/ui/render-card.js
+++ src/ui/render-card.js
@@ -5,7 +5,11 @@ import { Card, Player } from '../models';
  * @param {Player} jugador
  */
 export const renderCard = (card, jugador) => {
+  if (!card || !jugador)
+    throw new Error('Parameters card and jugador are mandatory');
+
   const cardsDeck = document.querySelector(`.${jugador}-cards-deck`);
+  if (!cardsDeck) throw new Error('jugador deck cannot be found');
 
   const cardImg = document.createElement('img');
   cardImg.classList.add('cards');
--- test/ui/render-card.test.js
+++ test/ui/render-card.test.js
@@ -1,5 +1,35 @@
+import { screen } from '@testing-library/dom';
+import { renderCard } from '../../src/ui';
+import { testCard } from '../fixtures/test-deck';
+import { testPlayer1 } from '../fixtures/test-players';
+
 describe('Test renderCard', () => {
-  test('should first', () => {
-    second;
+  test('should render cards correctly', () => {
+    const cardsDeck = document.createElement('div');
+    cardsDeck.classList.add(`${testPlayer1.id}-cards-deck`);
+    document.body.appendChild(cardsDeck);
+
+    renderCard(testCard.cardType, testPlayer1.id);
+    const cardImg = document.querySelector('.cards');
+
+    // Utilizando custom jest matchers de @testing-library/jest-dom toBeInTheDocument
+    expect(cardImg).toBeInTheDocument();
+    expect(cardImg.getAttribute('src')).toBe(
+      `../../public/cartas/${testCard.cardType}.png`
+    );
+  });
+
+  test('should throw an Error if the parameters are not sent', () => {
+    expect(() => renderCard()).toThrow();
+  });
+
+  test('should throw an Error if player deck cannot be found', () => {
+    const cardsDeck = document.createElement('div');
+    // cardsDeck.classList.add(`${testPlayer1.id}-cards-deck`); // class assignment omitted
+    document.body.appendChild(cardsDeck);
+
+    expect(() => renderCard(testCard.cardType, testPlayer1.id)).toThrow(
+      'jugador deck cannot be found'
+    );
   });
 });
