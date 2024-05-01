6c85a872eb9f982577c9d4fe961a70fa9f753c94
Susi <102126517+susigith@users.noreply.github.com>
2024-05-01 09:01:47 -0400

    [REFAC] paths

--- src/ui/render-card.js
+++ src/ui/render-card.js
@@ -5,15 +5,14 @@ import { Card, Player } from '../models';
  * @param {Player} jugador
  */
 export const renderCard = (card, jugador) => {
-  if (!card || !jugador)
-    throw new Error('Parameters card and jugador are mandatory');
+  if (!card || !jugador) throw new Error('Parameters card and jugador are mandatory');
 
   const cardsDeck = document.querySelector(`.${jugador}-cards-deck`);
   if (!cardsDeck) throw new Error('jugador deck cannot be found');
 
   const cardImg = document.createElement('img');
   cardImg.classList.add('cards');
-  cardImg.src = `../../public/cartas/${card}.png`;
+  cardImg.src = `../../cartas/${card}.png`;
 
   cardsDeck.appendChild(cardImg);
 };
